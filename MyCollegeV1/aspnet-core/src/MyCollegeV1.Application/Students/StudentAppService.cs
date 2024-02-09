b0ac77a909efcd1f76f6a7689553ea7e8db76351
FoysalAhamedSifat <foysalahamedsifat@gmail.com>
2024-02-09 05:27:38 -0500

    12. Test the CRUD functionality

--- MyCollegeV1/aspnet-core/src/MyCollegeV1.Application/Students/StudentAppService.cs
+++ MyCollegeV1/aspnet-core/src/MyCollegeV1.Application/Students/StudentAppService.cs
@@ -8,7 +8,7 @@ using MyCollegeV1.Models;
 namespace MyCollegeV1.Students
 {
 
-    // [AbpAuthorize(PermissionNames.Pages_Students)]
+    [AbpAuthorize(PermissionNames.Pages_Students)]
     public class StudentAppService : AsyncCrudAppService<Student, StudentDto, int, PagedStudentResultRequestDto, CreateStudentDto, UpdateStudentDto>, IStudentAppService
     {
         public StudentAppService
