00a04138443bfc395a9b8063cbd3c0e89c49148c
FoysalAhamedSifat <foysalahamedsifat@gmail.com>
2024-02-08 23:41:01 -0500

    3. Download the ASP.NET Boilerplate - done

---
+++ MyCollegeV1/aspnet-core/src/MyCollegeV1.Core/MyCollegeV1Consts.cs
@@ -0,0 +1,20 @@
+﻿using MyCollegeV1.Debugging;
+
+namespace MyCollegeV1
+{
+    public class MyCollegeV1Consts
+    {
+        public const string LocalizationSourceName = "MyCollegeV1";
+
+        public const string ConnectionStringName = "Default";
+
+        public const bool MultiTenancyEnabled = true;
+
+
+        /// <summary>
+        /// Default pass phrase for SimpleStringCipher decrypt/encrypt operations
+        /// </summary>
+        public static readonly string DefaultPassPhrase =
+            DebugHelper.IsDebug ? "gsKxGZ012HLL3MI5" : "2a6eadcf800b49adbdd315829bd9a32b";
+    }
+}
